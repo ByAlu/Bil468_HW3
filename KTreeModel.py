d8195740c331e6a357cb2d925a7c043867cbe654
Ali Belikirik <alibelikirik@gmail.com>
2026-07-12 14:47:14 +0300

    fix: tree create is now like a tree instead of array like

--- KTreeModel.py
+++ KTreeModel.py
@@ -16,27 +16,58 @@ class KTree:
     def create_tree(self , k, d):
         start = time.time()
 
-        k_means_models = []
+        k_means_models = [np.empty(k**i, dtype=object) for i in range(d)]
         cluster_label = np.zeros((len(self.train_x),d))
-        for i in range(d):
-            if(i == 0):
-                tree = KMeans(n_clusters=k, random_state=42)
-                y_pred = tree.fit_predict(self.train_x)
-                k_means_models.append(tree)
-                cluster_label[:,i] = y_pred
-            else:
-                models = []
-                for i in range(k):
-                    tree = KMeans(n_clusters=k, random_state=42)
-                    mask = self.train_x == i
-                    y_pred = tree.fit_predict(mask)
-                    cluster_label[:,i] = y_pred
-                    models.append(tree)
-                k_means_models.append(models)
-        
+     
+        self.create_tree_recursive(k, d, 0, 0, 0, [], k_means_models, cluster_label)
+
         elapsed_time = time.time() - start
 
         return k_means_models, cluster_label, elapsed_time
+
+    def create_tree_recursive(self, k, d, parent_model_index, model_index, d_val, masks, k_means_models, cluster_label):
+        if d_val >= d:
+            return
+
+        if(d_val == 0):
+            tree = KMeans(n_clusters=k, random_state=42)
+            y_pred = tree.fit_predict(self.train_x)
+            k_means_models[0][0] = tree
+            cluster_label[:,0] = y_pred
+            for i in range(k):
+                mask = y_pred == i
+                self.create_tree_recursive(k, d, 0, i, 1, [mask], k_means_models, cluster_label)
+        
+        else:
+            indices = np.arange(len(self.train_x))
+            for mask in masks:
+                indices = indices[mask]
+            
+            tree = KMeans(n_clusters=k, random_state=42)
+            y_pred = tree.fit_predict(self.train_x[indices])
+            cluster_label[indices,d_val] = y_pred
+
+            tree_index = parent_model_index * k + model_index
+            k_means_models[d_val][tree_index] = tree
+            for i in range(k):
+                mask = y_pred == i
+                new_masks = masks + [mask]
+                self.create_tree_recursive(k, d, tree_index, i, d_val + 1, new_masks, k_means_models, cluster_label)
+
+
+
+
+
+    # Araba için +1, diğerleri için -1. Total değer sıfırdan büyükse araba var deriz
+    def find_class_value(self, x, k_means_models, cluster_Label):
+        y = self.train_y
+        
+
+
+
+    def validate(k_means_models, cluster_Label):
+        s = 9
+
                             
 
 
